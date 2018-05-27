37acdc9994e5f3b3e6d8f148d4b519b187273e6d
anne-pham <phamanne25@gmail.com>
2018-05-26 20:19:09 -0700

    silly mistake

--- src/main/java/codeu/controller/AdminServlet.java
+++ src/main/java/codeu/controller/AdminServlet.java
@@ -100,12 +100,13 @@ public class AdminServlet extends HttpServlet {
         int numUsers = userStore.getAllUsers().size();
         int numCons = conStore.getAllConversations().size();
         int numMess = messStore.getAllMessages().size();
-        String newestUser = "Joanne Compo";
+        String newestUser = userStore.getNewestUser();
+
 
         request.setAttribute("numUsers", numUsers);
         request.setAttribute("numCons", numCons);
         request.setAttribute("numMess", numMess);
-        request.setAttribute("numUsers", newestUser);
+        request.setAttribute("newUser", newestUser);
         request.getRequestDispatcher("/WEB-INF/view/admin.jsp").forward(request, response);
 
   }
